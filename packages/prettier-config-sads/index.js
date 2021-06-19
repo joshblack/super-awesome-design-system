bf227036083d5724a30727ee94275e99a64d75ef
Josh Black <josh@josh.black>
2021-06-19 14:52:49 -0500

    chore(project): setup yarn workspace with prettier

---
+++ packages/prettier-config-sads/index.js
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) Super Awesome Design System
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+module.exports = {
+  jsxBracketSameLine: true,
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'es5',
+  proseWrap: 'always',
+};
