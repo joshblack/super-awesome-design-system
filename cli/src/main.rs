242e770a2963f21e9269723cf52be1b3962d21ab
Josh Black <josh@josh.black>
2021-06-20 13:39:15 -0500

    feat(cli): add initial command scaffold

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -4,12 +4,21 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+use env_logger::{fmt::Color, Builder};
+use log::{error, info, Level};
+use project::Project;
+use std::process;
 use structopt::StructOpt;
+
+mod project;
 
 #[derive(StructOpt)]
 struct Sads {
     #[structopt(subcommand)]
     cmd: Option<Subcommand>,
+
+    #[structopt(flatten)]
+    verbose: clap_verbosity_flag::Verbosity,
 }
 
 #[derive(StructOpt)]
@@ -19,6 +28,9 @@ enum Subcommand {
 
     #[structopt(about = "Create a new component or package")]
     Create(Create),
+
+    #[structopt(about = "initialize dsm to be used in your project")]
+    Init,
 }
 
 #[derive(StructOpt)]
@@ -29,28 +41,73 @@ enum Create {
     Package { name: String },
 }
 
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
 fn main() {
-    match Sads::from_args() {
-        Sads { cmd: None } => {
-            println!("Default command");
+    let Sads { cmd, verbose } = Sads::from_args();
+
+    init_logger(verbose.log_level());
+
+    if let Err(err) = try_main(cmd) {
+        error!("{}", err);
+        process::exit(1);
+    }
+}
+
+fn try_main(cmd: Option<Subcommand>) -> Result<()> {
+    let cwd = std::env::current_dir().expect("Expected to find current working directory");
+    match cmd {
+        None => {
+            info!("Default command");
+            let _project = Project::detect(cwd)?;
+            Ok(())
         }
 
-        Sads {
-            cmd: Some(Subcommand::Clean),
-        } => {
-            println!("Some command");
+        Some(Subcommand::Init) => {
+            info!("Initialize project in {:?}", cwd);
+            Ok(())
         }
 
-        Sads {
-            cmd: Some(Subcommand::Create(Create::Component { name })),
-        } => {
-            println!("Create component with name: {}", name);
+        Some(Subcommand::Clean) => {
+            info!("Some command");
+            let _project = Project::detect(cwd)?;
+            Ok(())
         }
 
-        Sads {
-            cmd: Some(Subcommand::Create(Create::Package { name })),
-        } => {
-            println!("Create package with name: {}", name);
+        Some(Subcommand::Create(Create::Component { name })) => {
+            info!("Create component with name: {}", name);
+            let _project = Project::detect(cwd)?;
+            Ok(())
         }
+
+        Some(Subcommand::Create(Create::Package { name })) => {
+            info!("Create package with name: {}", name);
+            let _project = Project::detect(cwd)?;
+            Ok(())
+        }
+    }
+}
+
+fn init_logger(level: Option<Level>) {
+    if let Some(level) = level {
+        let mut builder = Builder::from_default_env();
+
+        builder
+            .filter(None, level.to_level_filter())
+            .format(move |buf, record| {
+                use std::io::Write;
+
+                let mut style = buf.style();
+                let formatted = match level {
+                    Level::Trace => style.set_color(Color::Magenta).value("trace"),
+                    Level::Debug => style.set_color(Color::Blue).value("debug"),
+                    Level::Info => style.set_color(Color::Green).value("info"),
+                    Level::Warn => style.set_color(Color::Yellow).value("warn"),
+                    Level::Error => style.set_color(Color::Red).value("error"),
+                };
+
+                writeln!(buf, "{} {}", formatted, record.args())
+            })
+            .init()
     }
 }
