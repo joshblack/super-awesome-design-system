66433ae201b302f4e5168af10fc58a39efea2d82
Josh Black <josh@josh.black>
2021-10-11 12:37:22 -0500

    feat(project): add babel-preset-sads

---
+++ packages/babel-preset-sads/index.js
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) Super Awesome Design System
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+module.exports = () => {
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          modules: false,
+          targets: {
+            browsers: ['last 1 versions'],
+          },
+        },
+      ],
+      '@babel/preset-react',
+      '@babel/preset-typescript',
+    ],
+    plugins: [
+      'dev-expression',
+      '@babel/plugin-transform-react-constant-elements',
+    ],
+  };
+};
