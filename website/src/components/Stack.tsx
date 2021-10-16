873c182a2abc119a04a8b35663f9ce7d6d2d22e7
Josh Black <josh@josh.black>
2021-10-16 16:58:45 -0500

    chore(website): comment out stack component

--- website/src/components/Stack.tsx
+++ website/src/components/Stack.tsx
@@ -5,37 +5,40 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import cx from 'classnames';
-import * as React from 'react';
-
-type Props<T extends React.ReactNode> = {
-  as: T;
-  children: React.ReactNode;
-  spacing: number | string;
-  orientation: 'horizontal' | 'vertical';
-};
-
-const Stack = React.forwardRef(function Stack<T>(
-  props: Props<T>,
-  ref: React.ForwardedRef<T>
-) {
-  const {
-    as: BaseComponent = 'div',
-    children,
-    spacing,
-    orientation,
-    ...rest
-  } = props;
-  const className = cx({
-    hstack: orientation === 'horizontal',
-    vstack: orientation === 'vertical',
-  });
-
-  return (
-    <BaseComponent {...rest} className={className} ref={ref}>
-      {children}
-    </BaseComponent>
-  );
-});
+function Stack() {
+  return null;
+}
 
 export { Stack };
+
+// type Props<T extends React.ReactNode> = {
+// as: T;
+// children: React.ReactNode;
+// spacing: number | string;
+// orientation: 'horizontal' | 'vertical';
+// };
+
+// const Stack = React.forwardRef(function Stack<T>(
+// props: Props<T>,
+// ref: React.ForwardedRef<T>
+// ) {
+// const {
+// as: BaseComponent = 'div',
+// children,
+// spacing,
+// orientation,
+// ...rest
+// } = props;
+// const className = cx({
+// hstack: orientation === 'horizontal',
+// vstack: orientation === 'vertical',
+// });
+
+// return (
+// <BaseComponent {...rest} className={className} ref={ref}>
+// {children}
+// </BaseComponent>
+// );
+// });
+
+// export { Stack };
